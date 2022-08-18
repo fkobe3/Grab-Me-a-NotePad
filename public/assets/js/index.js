c2f0acf773532187a8d0f9c7eb1b568d138d1f93
Frank Kobe <fkobe3@gmail.com>
2022-08-18 08:22:49 -0400

    seeing if there is a node install issue

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -26,7 +26,7 @@ const hide = (elem) => {
 let activeNote = {};
 
 const getNotes = () =>
-  fetch('/api/multinote.html', {
+  fetch('/api/multinote', {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
@@ -34,7 +34,7 @@ const getNotes = () =>
   });
 
 const saveNote = (note) =>
-  fetch('/api/multinote.html', {
+  fetch('/api/multinote', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -43,7 +43,7 @@ const saveNote = (note) =>
   });
 
 const removeNote = (id) =>
-  fetch(`/api/multinote.html/${id}`, {
+  fetch(`/api/multinote/${id}`, {
     method: 'DELETE',
     headers: {
       'Content-Type': 'application/json',
--- routes/apiRoute/noteRoute.js
+++ routes/apiRoute/noteRoute.js
@@ -1,14 +1,14 @@
 const router = require('express').Router();
 const notes = require('../../db/db');
-const { createNote, removeNote, validateNote } = require('../../lib/multinote.js');
+const { createNote, removeNote, validateNote } = require('../../lib/multinote');
 
 // API response for GET methods on notes endpoint
-router.get('/multinote.js', (req, res) => {
+router.get('/multinote', (req, res) => {
     res.json(notes);
 });
 
 // API response for POST methods on notes endpoint
-router.post('/multinote.js', (req, res) => {
+router.post('/multinote', (req, res) => {
     if(validateNote(req.body)) {
         const note = createNote(notes, req.body);
         res.json(note);
@@ -18,7 +18,7 @@ router.post('/multinote.js', (req, res) => {
 });
 
 // API response for DELETE methods on notes endpoint
-router.delete('/multinote.js/:id', (req, res) => {
+router.delete('/multinote/:id', (req, res) => {
     const result = removeNote(notes, req.params.id);
 
     if(result)
