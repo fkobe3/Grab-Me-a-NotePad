d3ab213a59c13858dad107db099748c19e437fe2
Frank Kobe <fkobe3@gmail.com>
2022-08-18 06:54:27 -0400

    did note resolve, still attempting resolutions

--- routes/htmlRoute/index.js
+++ routes/htmlRoute/index.js
@@ -5,8 +5,8 @@ router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
 
-router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/notes.html'));
+router.get('/multinote', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/multinote.html'));
 });
 
 router.get('*', (req, res) => {
